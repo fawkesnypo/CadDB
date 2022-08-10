4e1c339b692e3ab49de9a8e36964454a4e5315b2
renan <renanflorencio689@gmail.com>
2022-08-10 00:11:47 -0300

    Criado database Criado conexão com database local em form Main Criado método para cadastro

--- backup/cadastro.pas
+++ backup/cadastro.pas
@@ -5,14 +5,16 @@ unit cadastro;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls,
-  StdCtrls, EditBtn;
+  Classes, SysUtils, sqldb, sqlite3conn, IBConnection, Forms, Controls,
+  Graphics, Dialogs, Menus, ExtCtrls, StdCtrls, EditBtn, DBCtrls,dateutils;
 
 type
 
   { TCadastroForm }
 
   TCadastroForm = class(TForm)
+    BtnCadastrar: TButton;
+    BtnLimpar: TButton;
     DateEdit1: TDateEdit;
     EIdade: TEdit;
     ENome: TEdit;
@@ -24,6 +26,8 @@ type
     RadioButtonF: TRadioButton;
     RadioButtonM: TRadioButton;
     RadioGroup1: TRadioGroup;
+    procedure BtnCadastrarClick(Sender: TObject);
+    procedure DateEdit1Exit(Sender: TObject);
   private
 
   public
@@ -36,6 +40,29 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TCadastroForm }
+
+procedure TCadastroForm.DateEdit1Exit(Sender: TObject);
+begin
+       EIdade.Text:=Trunc(dateutils.YearsBetween(Now,DateEdit1.Date)).toString;
+end;
+
+procedure TCadastroForm.BtnCadastrarClick(Sender: TObject);
+begin
+
+     if (ENome.GetTextLen() > 3) and (Length(DateEdit1.GetTextLen() > 6) and (EIdade.GetTextLen() > 0) and (RadioButtonF.IsEnabled or RadioButtonM.IsEnabled) then
+
+        //main.TMainForm.DBCnn.ExecuteDirect(
+        //'INSERT INTO usuario(nome,dtNascimento,idade,sexo)'+
+        //'VALUES('+
+        //);
+
+        ShowMessage('Okay')
+     else
+         ShowMessage('É necessário preencher todos os campos');
+end;
+
 
 end.
 
--- backup/main.pas
+++ backup/main.pas
@@ -5,7 +5,8 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus,cadastro;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, cadastro,
+  consulta, sqlite3conn, sqldb,sqlite3dyn;
 
 type
 
@@ -16,7 +17,11 @@ type
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
+    DBCnn: TSQLite3Connection;
+    DBTr: TSQLTransaction;
+    procedure FormCreate(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
   private
 
   public
@@ -35,9 +40,47 @@ implementation
 procedure TMainForm.MenuItem2Click(Sender: TObject);
 begin
      if cadastro.CadastroForm.Showing then
-        Continue
-
+        cadastro.CadastroForm.SetFocus
      else cadastro.CadastroForm.Show();
 end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+var
+  database:string;
+begin
+     InitializeSqlite('libsqlite3.so.0');
+     database := 'database.db';
+     if not FileExists(database) then
+        begin
+          try
+            DBCnn.DatabaseName:=database;
+            DBCnn.Connected:=True;
+
+            DBTr.DataBase:= DBCnn;
+            DBTr.Active:=True;
+            DBCnn.ExecuteDirect(
+              'CREATE TABLE usuario (  '+
+              '     id INTEGER PRIMARY KEY AUTOINCREMENT,'+
+              '     nome VARCHAR (30),'+
+              '     dtNascimento  DATE,'+
+              '     idade INTEGER '+
+              '     sexo VARCHAR (1)'+
+              ' ); '
+            );
+
+            DBTr.CommitRetaining;
+          except
+            ShowMessage('Não foi possível criar o database: '+ database);
+          end;
+        end;
+end;
+
+procedure TMainForm.MenuItem3Click(Sender: TObject);
+begin
+     if consulta.ConsutaForm.Showing then
+        consulta.ConsutaForm.SetFocus
+     else consulta.ConsutaForm.Show();
+end;
+
 end.
 
