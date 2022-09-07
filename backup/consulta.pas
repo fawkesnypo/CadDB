915caf372ee393911503a29e91af5268a67af696
Renan Florencio <renanflorencio689@gmail.com>
2022-09-07 16:46:14 -0300

    Alterações para Cadastro e consulta
    Cadastro: Adicionado connector na tela de cadastro
    Cadastro: implementado lógica para cadastro na tabela
    Consulta: Implementado lógica de consulta para buscar nomes cadastrados

--- backup/consulta.pas
+++ backup/consulta.pas
@@ -5,7 +5,8 @@ unit consulta;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+  Classes, SysUtils,SQLDB, SQLite3Conn,SQLite3Dyn, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, StdCtrls;
 
 type
 
@@ -14,6 +15,8 @@ type
   TConsutaForm = class(TForm)
     BtnConsultar: TButton;
     CbbNome: TComboBox;
+    DBCnn: TSQLite3Connection;
+    DBTr: TSQLTransaction;
     EIdade: TEdit;
     ENascimento: TEdit;
     LIdade: TLabel;
@@ -21,6 +24,7 @@ type
     LConsultar: TLabel;
     LNome: TLabel;
     Panel1: TPanel;
+    procedure FormActivate(Sender: TObject);
   private
 
   public
@@ -33,6 +37,36 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TConsutaForm }
+
+procedure TConsutaForm.FormActivate(Sender: TObject);
+var
+  database:string;
+  query:TSQLQuery;
+begin
+     InitializeSqlite('sqlite3.dll');
+     database := 'database.db';
+     if not FileExists(database) then
+        ShowMessage('A base de dados não foi encontrada!')
+     else
+       DBCnn.DatabaseName:=database;
+       DBCnn.Connected:=True;
+
+     query := TSQLQuery.Create(nil);
+     query.SQL.Text:='SELECT nome From usuario;';
+     query.DataBase := DBCnn;
+     query.Open;
+
+     while not query.EOF do
+     begin
+       if CbbNome.Items.IndexOf(query.FieldByName('nome').AsString) = -1 then;
+          CbbNome.Items.Add(query.FieldByName('nome').AsString);
+       query.Next();
+     end;
+     query.Close();
+     DBCnn.Close();
+end;
 
 end.
 
--- main.pas
+++ main.pas
@@ -17,8 +17,6 @@ type
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
-    DBTr: TSQLTransaction;
-    procedure FormCreate(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
   private
@@ -41,37 +39,6 @@ begin
      if cadastro.CadastroForm.Showing then
         cadastro.CadastroForm.SetFocus
      else cadastro.CadastroForm.Show();
-end;
-
-procedure TMainForm.FormCreate(Sender: TObject);
-var
-  database:string;
-begin
-     InitializeSqlite('sqlite3.dll');
-     database := 'database.db';
-     if not FileExists(database) then
-        begin
-          try
-            DBCnn.DatabaseName:=database;
-            DBCnn.Connected:=True;
-
-            DBTr.DataBase:= DBCnn;
-            DBTr.Active:=True;
-            DBCnn.ExecuteDirect(
-              'CREATE TABLE usuario (  '+
-              '     id INTEGER PRIMARY KEY AUTOINCREMENT,'+
-              '     nome VARCHAR (30),'+
-              '     dtNascimento  DATE,'+
-              '     idade INTEGER '+
-              '     sexo VARCHAR (1)'+
-              ' ); '
-            );
-
-            DBTr.CommitRetaining;
-          except
-            ShowMessage('Não foi possível criar o database: '+ database);
-          end;
-        end;
 end;
 
 procedure TMainForm.MenuItem3Click(Sender: TObject);
